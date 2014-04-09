5b30d2e176498d54845368fecf390b153bc1d08d
Melvin <m.yang0204@gmail.com>
2014-04-09 12:19:51 +0800

    init project
    init project

---
+++ MYTableViewManager/MYTableViewManager/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MYTableViewManager
+//
+//  Created by Melvin on 4/9/14.
+//  Copyright (c) 2014 Melvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MYTableViewManager/MYTableViewManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MYTableViewManager
+//
+//  Created by Melvin on 4/9/14.
+//  Copyright (c) 2014 Melvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
