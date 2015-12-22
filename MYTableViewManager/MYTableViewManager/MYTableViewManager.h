aa0524324f5b8f01bccaccda3f8b1e9eb256fb6d
Melvin <m.yang0204@gmail.com>
2015-12-22 19:48:21 +0800

    will load cell
    will load cell

--- MYTableViewManager/MYTableViewManager/MYTableViewManager.h
+++ MYTableViewManager/MYTableViewManager/MYTableViewManager.h
@@ -83,7 +83,7 @@
  @param cell A table-view cell object that tableView is going to create.
  @param indexPath An index path locating the row in tableView.
  */
-- (void)tableView:(UITableView *)tableView willLoadCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath;
+- (void)tableView:(UITableView *)tableView willLoadCell:(MYTableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath;
 
 /*
  Tells the delegate the table view has created a cell for a particular row and made it reusable.
@@ -92,6 +92,6 @@
  @param cell A table-view cell object that tableView has created.
  @param indexPath An index path locating the row in tableView.
  */
-- (void)tableView:(UITableView *)tableView didLoadCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath;
+- (void)tableView:(UITableView *)tableView didLoadCell:(MYTableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath;
 
 @end
